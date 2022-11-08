69297f0d73dbc680793fe8df77bea7fdfdec0f73
Mateus Abdias <mateusabdias.dev@gmail.com>
2022-11-07 22:48:04 -0300

    first commit

---
+++ aula02/index.js
@@ -0,0 +1,5 @@
+const meu_Modulo = require('./meuModulo');
+const soma = meu_Modulo.soma
+
+soma(10, 2)
+soma(5, 5)
---
+++ aula03/index.mjs
@@ -0,0 +1,5 @@
+import soma from './meuModulo.mjs'
+
+soma(10, 1)
+soma(1, 1)
+soma(3, 9)
